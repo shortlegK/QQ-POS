27a47b2daa4b84394f85ff2e80cfb89727bf6a74
shortleg <hsuehyikuo@gmail.com>
2025-11-27 11:53:52 +0800

    chore: 初始化專案結構

---
+++ src/main/java/com/qqriceball/QqPosApplication.java
@@ -0,0 +1,13 @@
+package com.qqriceball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QqPosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qqriceball/QqPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qqriceball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QqPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
