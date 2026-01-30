ef8354f63885827bb7ec3cfd1e01d6bd34e7685e
shortleg <hsuehyikuo@gmail.com>
2026-01-30 17:58:41 +0800

    feat(database): 新增資料庫結構與本機測試資料腳本
    - 新增 db/qqPosDB.sql，建立資料表結構
    - 新增 db/seedUserTest.sql，提供預設測試帳號

---
+++ db/qqPosDB.sql
@@ -0,0 +1,104 @@
+create table if not exists emp
+(
+    id          int unsigned auto_increment comment '員工編號'
+        primary key,
+    username    varchar(20)                                not null comment '帳號',
+    password    varchar(100)                               not null comment '密碼雜湊',
+    name        varchar(20)                                not null comment '姓名',
+    role        tinyint unsigned default '1'               not null comment '角色權限',
+    status      tinyint unsigned default '1'               not null comment '啟用狀態(1=啟用,0=停用)',
+    entry_date  date                                       not null comment '入職日期',
+    create_id   int unsigned                               not null,
+    create_time datetime         default CURRENT_TIMESTAMP not null comment '建立時間',
+    update_id   int unsigned                               not null,
+    update_time datetime         default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新時間',
+    constraint emp_pk_username
+        unique (username)
+)
+    comment '員工表';
+
+create table if not exists order_detail
+(
+    id                int unsigned auto_increment comment 'ID'
+        primary key,
+    order_id          varchar(20)  not null comment '訂單編號',
+    product_type      int unsigned not null comment '0 - 葷食, 1 -  素食, 2 - 飲料',
+    product_id        int unsigned not null comment '產品 ID',
+    product_price     int unsigned not null comment '單一商品售價快照（未含額外選項）',
+    quantity          int unsigned not null comment '訂購數量',
+    line_total_amount int unsigned not null comment '項目總金額(包含 order_detail_option)'
+)
+    comment '訂單明細表';
+
+create table if not exists order_detail_option
+(
+    order_detail_id int unsigned             not null comment '訂單明細 ID',
+    option_id       int unsigned             not null comment '額外選項 ID',
+    option_price    int unsigned default '0' not null comment '單一選項售價快照',
+    quantity        int unsigned default '1' not null comment '訂購數量',
+    primary key (option_id,
+                 order_detail_id)
+)
+    comment '額外選項明細表';
+
+create table if not exists orders
+(
+    id             int unsigned auto_increment comment 'id'
+        primary key,
+    order_id       varchar(20)                                not null comment '訂單編號',
+    operate_emp_id int unsigned                               not null comment '操作人員 ID',
+    amount         int unsigned                               not null comment '訂單金額',
+    status         tinyint unsigned default '0'               not null comment '訂單狀態(0 - 未完成, 1 - 已完成, 2 - 已取消)',
+    create_id      int unsigned                               not null,
+    create_time    datetime         default CURRENT_TIMESTAMP not null comment '建立時間',
+    update_id      int unsigned                               not null,
+    update_time    datetime         default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新時間',
+    constraint orders_pk_2
+        unique (order_id)
+)
+    comment '訂單表';
+
+create table if not exists product
+(
+    id           int unsigned auto_increment comment '產品 id'
+        primary key,
+    title        varchar(20)                                not null comment '產品名稱',
+    product_type tinyint unsigned                           not null comment '產品類型 (0 - 葷飯糰,1 - 素飯糰 ,2 - 飲品 )',
+    price        int unsigned                               not null comment '價格',
+    status       tinyint unsigned default '1'               not null comment '啟用狀態(1=啟用,0=停用)',
+    create_id    int unsigned                               not null,
+    create_time  datetime         default CURRENT_TIMESTAMP not null,
+    update_id    int unsigned                               not null,
+    update_time  datetime         default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新時間',
+    constraint product_pk_title
+        unique (title)
+);
+
+create table if not exists product_option
+(
+    id          int unsigned auto_increment comment 'id'
+        primary key,
+    title       varchar(20)                                not null comment '選項名稱',
+    option_type tinyint unsigned                           not null comment '選項類型(0 - 米飯種類, 1 - 飯量, 2 - 辣度, 3 - 加料)',
+    price       int unsigned     default '0'               not null comment '價格',
+    status      tinyint unsigned default '1'               not null comment '啟用狀態(1=啟用,0=停用)',
+    create_id   int unsigned                               not null,
+    create_time datetime         default CURRENT_TIMESTAMP not null comment '建立時間',
+    update_id   int unsigned                               not null,
+    update_time datetime         default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新時間',
+    constraint product_option_pk_2
+        unique (title,
+                option_type)
+)
+    comment '額外選項表';
+
+create table if not exists product_option_link
+(
+    product_id int unsigned not null comment '產品 id',
+    option_id  int unsigned not null comment '額外選項 id ',
+    primary key (product_id,
+                 option_id)
+)
+    comment '產品、額外選項中間表';
+
+
